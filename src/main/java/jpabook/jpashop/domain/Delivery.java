f12e0ebea1efadaac38913efac454537f1cca4b5
kimtimo <eogus8875@naver.com>
2024-01-17 01:20:43 +0900

    [추가] enum 추가, 엔티티 도메인 설계 중간 저장

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,4 +1,28 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
 public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    /**
+     * EnumType ORDINAL 절대금지
+     * DB에 들어갈 때 index 번호로 들어가는데 만약, READY와 COMP 사이에 상태값이 생긴다면 망한다.
+     * 반드시 STRING으로 지정할 것
+     */
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;    // READY, COMP
 }
