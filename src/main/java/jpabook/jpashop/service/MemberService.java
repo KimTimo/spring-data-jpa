c9ad5c71284d2fdee4e5554466af7d98d3166369
kimtimo <eogus8875@naver.com>
2024-04-04 00:16:46 +0900

    feat : 회원 조회 로직 작성
     - 회원 전체 조회, 한 건 조회 메서드 작성

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MemberService {
 
@@ -20,10 +22,27 @@ public class MemberService {
         return member.getId();
     }
 
+    /**
+     * 중복 회원 검증
+     */
     private void validateDuplicateMember(Member member) {
-        // EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
     }
 
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
 
-    //회원 전체 조회
+    /**
+     * 한 건 조회
+     */
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
 }
